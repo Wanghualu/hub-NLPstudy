90d71adfca0e2fe829a0f37f4144671dfee16c9f
wynstrive <87252773+wynstrive@users.noreply.github.com>
2026-04-30 19:45:37 +0800

    Fix syntax and logic errors in 深度学习组件

--- 王亚楠/week03_深度学习组件/作业一.py
+++ 王亚楠/week03_深度学习组件/作业一.py
@@ -5,10 +5,10 @@ import random
 import json
 
 # 1. 模型
-class TorchModel(nn.Model):
+class TorchModel(nn.Module):
   def __init__(self, vector_dim, sentence_length, vocab):
     super(TorchModel, self).__init__()
-    self.embedding = nn.Embedding(len(vocab, vector_dim, padding_idx = 0))
+    self.embedding = nn.Embedding(len(vocab), vector_dim, padding_idx = 0)
     self.RNN = nn.RNN(vector_dim, hidden_size = 128, batch_first = True)
     self.hidden_size = 128
     self.classify = nn.Linear(128, sentence_length + 2)
@@ -16,6 +16,8 @@ class TorchModel(nn.Model):
   def forward(self,x, y = None):
     x = self.embedding(x)
     rnn_out, hidden = self.RNN(x)
+    print(f"rnn_out.shape:{rnn_out.shape}")
+    print(f"hidden:{hidden}")
     x = rnn_out[:, -1, :]
     y_pred = self.classify(x)
     if y is not None:
@@ -46,7 +48,7 @@ def build_sample(vocab, sentence_length):
 # 3. 建立数据集
 def build_dataset(sample_length, vocab, sentence_length):
   dataset_x = []
-  datasey_y = []
+  dataset_y = []
   for i in range(sample_length):
     x, y = build_sample(vocab, sentence_length)
     dataset_x.append(x)
@@ -68,7 +70,7 @@ def evaluate(model, vocab, sample_length):
     preds = torch.argmax(y_pred, dim = -1)
     correct = (preds == y).sum().item()
     wrong = len(y) - correct
-  print('正确预测个数：%d, 正确率： %f' % (correct, corrcet / (correct + wrong)))
+  print('正确预测个数：%d, 正确率： %f' % (correct, correct / (correct + wrong)))
   return correct / (correct + wrong)
 
 # 6. 主程序
@@ -79,7 +81,7 @@ def main():
   char_dim = 20
   sentence_length = 6
   learning_rate = 0.005
-  vocab = build_voacb()
+  vocab = build_vocab()
   model = build_model(vocab, char_dim, sentence_length)
   optim = torch.optim.Adam(model.parameters(), lr = learning_rate)
   log = []
@@ -93,13 +95,13 @@ def main():
       loss.backward()
       optim.step()
       watch_loss.append(loss.item())
-    print("==========\n第%d轮平均loss:%f" % (epoch + 1), np.mean(watch_loss))
+    print("==========\n第%d轮平均loss:%f" % (epoch + 1, np.mean(watch_loss)))
     acc = evaluate(model, vocab, sentence_length)
     log.append([acc, np.mean(watch_loss)])
   torch.save(model.state_dict(), "week3model.pth")
   writer = open("vocab.json", "w", encoding = "utf8")
-  write.write(json.dumps(vocab, ensure_ascii = False, indent = 2))
-  write.close()
+  writer.write(json.dumps(vocab, ensure_ascii = False, indent = 2))
+  writer.close()
   return
 
 # 7. 预测
@@ -115,7 +117,7 @@ def predict(model_path, vocab_path, input_strings):
   model.eval()
   with torch.no_grad():
     result = model.forward(torch.LongTensor(x))
-  for i, input_string in enumeate(input_string):
+  for i, input_string in enumerate(input_string):
     prob, class_index = torch.max(result[i], dim = -1)
     print("输入：%s, 预测类别索引：%d, 置信度：%f" % (input_string, class_index.item(), prob.item()))
 if __name__ == "__main__":
